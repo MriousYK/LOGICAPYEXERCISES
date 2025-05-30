a6b996e74a89e6b591b6b92213a726f727552044
MriousYK <alvaradomarcoslevi@gmail.com>
2025-05-30 17:38:32 -0500

    sum loop en ingles

--- Sum_loop.py
+++ Sum_loop.py
@@ -1,9 +1,8 @@
-
 s = 0
-contador = int(input("Numero de sumas: "))
-while contador > 0:
-    numero = int(input("Numero a sumar: "))
-    s = s + numero
-    contador -= 1 #* esto es igual a decir contador = contador + 1
-    print(f"Resultado: {s}")
-    print(f"Iteraciones restantes: {contador}")   
+counter = int(input("Number of sums: "))
+while counter > 0:
+    number = int(input("Number to add: "))
+    s = s + number
+    counter -= 1  #*
+    print(f"Result: {s}")
+    print(f"Remaining iterations: {counter}")
